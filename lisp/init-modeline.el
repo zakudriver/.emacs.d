5d451437be7904773aec00422e2eb439ab30c10c
Zhan9Yunhua <zy.hua1122@qq.com>
2020-01-19 23:19:41 +0800

    modeline

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -9,39 +9,41 @@
   (nyan-animate-nyancat nil)
   (nyan-wavy-trail nil))
 
-(setq
- evil-normal-state-tag   (propertize " N " 'face '((:background "green" :foreground "black")))
- evil-emacs-state-tag    (propertize " E " 'face '((:background "orange" :foreground "black")))
- evil-insert-state-tag   (propertize " I " 'face '((:background "red") :foreground "white"))
- evil-motion-state-tag   (propertize " M " 'face '((:background "blue") :foreground "white"))
- evil-visual-state-tag   (propertize " V " 'face '((:background "grey80" :foreground "black")))
- evil-operator-state-tag (propertize " O " 'face '((:background "purple"))))
-
 ;; (setq
-;;  evil-normal-state-tag   (propertize " N " 'face '((:foreground "green")))
-;;  evil-emacs-state-tag    (propertize " E " 'face '((:foreground "orange")))
-;;  evil-insert-state-tag   (propertize " I " 'face '((:foreground "red")))
-;;  evil-motion-state-tag   (propertize " M " 'face '((:foreground "blue")))
-;;  evil-visual-state-tag   (propertize " V " 'face '((:foreground "grey80")))
-;;  evil-operator-state-tag (propertize " O " 'face '((:foreground "purple"))))
+;;  evil-normal-state-tag   (propertize "< N >" 'face '((:background "green" :foreground "black")))
+;;  evil-emacs-state-tag    (propertize "< E >" 'face '((:background "orange" :foreground "black")))
+;;  evil-insert-state-tag   (propertize "< I >" 'face '((:background "red") :foreground "white"))
+;;  evil-motion-state-tag   (propertize "< M >" 'face '((:background "blue") :foreground "white"))
+;;  evil-visual-state-tag   (propertize "< V >" 'face '((:background "grey80" :foreground "black")))
+;;  evil-operator-state-tag (propertize "< O >" 'face '((:background "purple"))))
+
+(setq
+ evil-normal-state-tag   (propertize "< N >" 'face 'font-lock-preprocessor-face)
+ evil-emacs-state-tag    (propertize "< E >" 'face 'font-lock-preprocessor-face)
+ evil-insert-state-tag   (propertize "< I >" 'face 'font-lock-preprocessor-face)
+ evil-motion-state-tag   (propertize "< M >" 'face 'font-lock-preprocessor-face)
+ evil-visual-state-tag   (propertize "< V >" 'face 'font-lock-preprocessor-face)
+ evil-operator-state-tag (propertize "< O >" 'face 'font-lock-preprocessor-face))
 
 
+;; ⓪ ① ② ③ ④ ⑤ ⑥ ⑦ ⑧ ⑨ ⑩
+;; Ⅰ Ⅱ Ⅲ Ⅳ Ⅴ Ⅵ Ⅶ Ⅷ Ⅸ Ⅹ
 (defun modeline-unicode-number (str)
   "Return a nice unicode representation of a single-digit number STR."
   (cond
-   ((string= "1" str) "➊")
-   ((string= "2" str) "➋")
-   ((string= "3" str) "➌")
-   ((string= "4" str) "➍")
-   ((string= "5" str) "➎")
-   ((string= "6" str) "➏")
-   ((string= "7" str) "➐")
-   ((string= "8" str) "➑")
-   ((string= "9" str) "➒")
-   ((string= "0" str) "➓")))
+   ((string= "1" str) " Ⅰ ")
+   ((string= "2" str) " Ⅱ ")
+   ((string= "3" str) " Ⅲ ")
+   ((string= "4" str) " Ⅳ ")
+   ((string= "5" str) " Ⅴ ")
+   ((string= "6" str) " Ⅵ ")
+   ((string= "7" str) " Ⅶ ")
+   ((string= "8" str) " Ⅷ ")
+   ((string= "9" str) " Ⅸ ")
+   ((string= "0" str) " Ⅹ ")))
 
 
-(defun mode-line-fill (face reserve)
+(defun modeline-fill (face reserve)
   "Return empty space using FACE and leaving RESERVE space on the right."
   (unless reserve
     (setq reserve 20))
@@ -74,7 +76,7 @@
               (face (cond (no-errors 'error)
                           (no-warnings 'warning)
                           (t 'success))))
-         (propertize (format "%s / %s" (or no-errors 0) (or no-warnings 0))
+         (propertize (format " † %s  ‡ %s " (or no-warnings 0) (or no-errors 0))
                      'face face)))
       (`interrupted " -")
       (`suspicious '(propertize " ?" 'face 'warning)))))
@@ -86,7 +88,7 @@
                          ;; winum
                          " "
                          '(:eval (propertize
-                                  (modeline-unicode-number (winum-get-number-string))))
+                                  (modeline-unicode-number (winum-get-number-string)) 'face 'font-lock-preprocessor-face))
                          " "
 
                          ;; is Modified
@@ -110,7 +112,7 @@
                          
                          ;; evil state
                          '(:eval evil-mode-line-tag)
-                         "  "
+                         "   "
                          
                          ;; '(:eval (propertize "[" 'face 'font-lock-type-face))
                          '(:eval (list (nyan-create)))
@@ -148,7 +150,7 @@
                          
                            ;; is this buffer read-only?
                            '(:eval (when buffer-read-only
-                                     (concat ","  (propertize "RO"
+                                     (concat ", "  (propertize "RO"
                                                               'face 'font-lock-type-face
                                                               'help-echo "Buffer is read-only"))))
                            " "
@@ -159,23 +161,22 @@
                         (modeline-right (list
                                          ;; global-mode-string goes in mode-line-misc-info
                                          mode-line-misc-info
-                                         
-                                         (mode-line-fill 'mode-line 20)
+
+                                         (modeline-fill 'mode-line 10)
                                          
                                          ;; line and column
                                          ;; '%02' to set to 2 chars at least; prevents flickering
                                          (propertize "%02l" 'face 'font-lock-type-face) ","
                                          (propertize "%02c" 'face 'font-lock-type-face)
-                                         " "
+                                         "  "
                                          
                                          '(:eval (buffer-encoding-abbrev))
-
-                                         mode-line-end-spaces
+                                         " "
                                          ))
                         (modeline-height (list
                                           (propertize " "
-                                                      'display '(height 1.3))
-                                          (propertize " " 'display '(raise -0.3))))
+                                                      'display '(height 1.4))
+                                          (propertize " " 'display '(raise -0.6))))
 
                         (width-left (string-width (format-mode-line modeline-left)))
                         (width-left-middle (string-width (format-mode-line (list modeline-left modeline-middle))))
@@ -190,8 +191,14 @@
 (setq-default mode-line-format '(:eval (modeline-renderer)))
 (set-face-attribute 'mode-line nil
                     :family "SF Pro Text"
-                    :height 95
-                    )
+                    :height 100
+                    :overline nil
+                    :underline nil)
+(set-face-attribute 'mode-line-inactive  nil
+                    :height 90
+                    :overline nil
+                    :underline nil
+                    :background (face-background 'default))
 
 (setq x-underline-at-descent-line t)
 
