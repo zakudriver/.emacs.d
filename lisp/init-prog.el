40e8714ec95274f1b13c7dafa86838f56c6c6ed6
Zhan9Yunhua <zy.hua1122@qq.com>
2020-02-01 22:16:30 +0800

    proto file

--- lisp/init-prog.el
+++ lisp/init-prog.el
@@ -1,12 +1,18 @@
 ;;; Code:
 
 (use-package vimrc-mode)
+
 (use-package dockerfile-mode)
+
 (use-package bazel-mode
   :mode
   (("\\.bzl$" . bazel-mode)
    ("\\.bazel" . bazel-mode)
    ("^WORKSPACE$" . bazel-mode)))
+
+(use-package protobuf-mode
+  :mode
+  (("\\.proto$" . protobuf-mode)))
 
 
 (provide 'init-prog)
